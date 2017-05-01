95a094da86b90993341bf69897b0a987ec88ccc8
Charles1104 <charles.xavier.verleyen@gmail.com>
2017-05-01 09:47:30 -1000

    Three out of five done

--- index.js
+++ index.js
@@ -8,10 +8,45 @@ function getData(index, cb) {
 
 // wrap getData in a promise and console log the output
 
+
+
 // call getData three times and console log the output (use .then for the second and third call) using promises
+
+var promise = getData(0);
+promise.then(function(data){
+  console.log(data);
+  return getData(1);
+}).then(function(data){
+  console.log(data);
+  return getData(2);
+}).then(function(data){
+  console.log(data);
+}).catch(function(error){
+  console.log(error);
+});
+
 
 // create a promise within getData and use the promise to trigger the callback in getData
 
+function getData(index) {
+  return new Promise(function(resolve, reject){
+    if(index >= 0 && index <= 2){
+      resolve(data[index]);
+    } else{
+      reject(new Error('index out of bounds'));
+    }
+  });
+}
+
 // create a promisified version of getData
 
+
+
 // call the promisified version of getData with -1, console log the error in a .catch
+
+var promise = getData(-1);
+promise.then(function(data){
+  console.log(data);
+}).catch(function(error){
+  console.log(error);
+});
